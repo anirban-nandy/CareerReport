28ad025cd6d65d343155c6e82dec927296815dd1
Anirban Nandy <anirban.nandy@technicise.com>
2013-04-22 19:06:28 +0530

    added UI for data Insertion

---
+++ CareerReport/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CareerReport
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
